b4375ed499499b6adcaa1083b423f540d5a9999a
GANGI JATHIN <137679350+Jathin4@users.noreply.github.com>
2026-09-07 05:56:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (88.99%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.py
@@ -0,0 +1,9 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        s = 0
+        for i in operations:
+            if "-" in i:
+                s += -1
+            else:
+                s += 1
+        return s
